e68f6b9dce7951b4a949b608563348a9d7b47ff6
Abdallah008 <abdallahjamshaid@yahoo.com>
2024-04-11 22:27:40 -0700

    Added assignments 2 & 3

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("Hello World!");
+// Assignment 1
+
+// alert("Hello World!");
+
+// Assignment 2
+
+let personName = "Ronaldo";
+let personAge = "39";
+let isacashier = false;
+let height = 187.5;
+let hobbies; 
+let address = null;
+
+console.log("Person's name is " + personName);
+console.log("Person's age is " + personAge);
+console.log("Is the person a cashier? " + isacashier);
+console.log("Person's height is " + height);
+console.log("Person's hobbies are" + hobbies)
+console.log("Person's address is " + address);
+
+// Assignment 3
+let todolist = ["Complete assigment", "Complete report", "pay bill"];
+let myPlaystation = { brand: "Sony", color: "White" };
+
+console.log("my first to do is: " + todolist[0]);
+console.log("my playstation brand is: " + myPlaystation.brand);
